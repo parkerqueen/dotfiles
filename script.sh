c5813a6c33f1fc7c2389affa379b76f5380377b3
parkerqueen <sobanr4@gmail.com>
2020-10-22 01:24:33 +0500

    Added Autosuggestions, Syntax Highlighting & Fonts

--- script.sh
+++ script.sh
@@ -9,6 +9,9 @@ chsh -s $(which zsh)
 
 # Oh My ZSH Setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo cp -r meslo-fonts/ /usr/share/fonts/truetype/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp -f .zshrc ~
 cp -f .p10k.zsh ~
